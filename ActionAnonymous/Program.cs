8fbcac408cb32a7b555925f4b750953a67c9f404
Bhabin-2000 <119649122+Bhabin-2000@users.noreply.github.com>
2022-12-08 09:07:08 +0530

    Add files via upload

---
+++ ActionAnonymous/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ActionAnonymous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Action Delegate Calculator ");
+
+            Action<int, int> Adding_Anonymous = delegate (int firstnum, int secondnum)
+               {
+                   Console.WriteLine("\n Adding a two values is : "+(firstnum+secondnum));
+               };
+
+
+
+            Action<int, int> Subract_Anonymous = delegate (int firstnum, int secondnum)
+            {
+                Console.WriteLine("\n Subracting a two values is : " + (firstnum - secondnum));
+            };
+
+
+            Action<int, int> Multiple_Anonymous = delegate (int firstnum, int secondnum)
+            {
+                Console.WriteLine("\n Multiplaying a two values is : " + (firstnum * secondnum));
+            };
+
+
+
+            Action<int, int> Division_Anonymous = delegate (int firstnum, int secondnum)
+            {
+                Console.WriteLine("\n Dividing a two values is : " + (firstnum / secondnum));
+            };
+
+
+            calculator(Adding_Anonymous+= Subract_Anonymous+= Multiple_Anonymous+= Division_Anonymous, 20, 4);
+
+            Console.ReadKey();
+
+        }
+        static void calculator(Action<int,int> cal, int First,int Second)
+        {
+            cal(First, Second);
+        }
+    }
+}
---
+++ Anonymus/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Anonymus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous");
+            Func<int, int, int> AnonymusAdd = delegate (int firstNum, int secondNum)
+               {
+                   return (firstNum + secondNum);
+               };
+            Calculator(AnonymusAdd);
+
+
+            Func<int, int, int> AnonymusSub = delegate (int firstNum, int secondNum)
+            {
+                return (firstNum - secondNum);
+            };
+            Calculator(AnonymusSub);
+
+
+            Func<int, int, int> AnonymusMultiple = delegate (int firstNum, int secondNum)
+            {
+                return (firstNum * secondNum);
+            };
+            Calculator(AnonymusMultiple);
+
+
+            Func<int, int, int> AnonymusDivision = delegate (int firstNum, int secondNum)
+            {
+                return (firstNum / secondNum);
+            };
+            Calculator(AnonymusDivision);
+        }
+
+        static void Calculator(Func<int,int,int> fun)
+        {
+            Console.WriteLine(fun(100, 120)); 
+        }
+        
+    }
+}
